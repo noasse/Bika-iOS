efd80704878f8835da1840a14c542f56f3d4253f
noasse <c1530415145@icloud.com>
2026-03-17 17:00:26 +0800

    Initial Commit

---
+++ bika/bikaApp.swift
@@ -0,0 +1,17 @@
+//
+//  bikaApp.swift
+//  bika
+//
+//  Created by noasse on 3/17/26.
+//
+
+import SwiftUI
+
+@main
+struct bikaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
